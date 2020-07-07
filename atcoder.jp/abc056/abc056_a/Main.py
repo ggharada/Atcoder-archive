1441b23a4e7c298fec97afb98837e52f960430f6
ggharada <ggharada39@gmail.com>
2020-07-07 11:50:37 +0900

    [AC] abc056 abc056_a

---
+++ atcoder.jp/abc056/abc056_a/Main.py
@@ -0,0 +1,9 @@
+a,b = map(str,input().split())
+if a == "D" and b == "D":
+    print("H")
+elif a == "H" and b == "H":
+    print("H")
+elif a == "D" and b == "H":
+    print("D")
+else:
+    print("D")
