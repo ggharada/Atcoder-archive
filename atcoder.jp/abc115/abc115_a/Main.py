45234b190932c0c10dcde9755e76e39f086fac96
ggharada <ggharada39@gmail.com>
2020-07-09 18:55:13 +0900

    [AC] abc115 abc115_a

---
+++ atcoder.jp/abc115/abc115_a/Main.py
@@ -0,0 +1,9 @@
+check = int(input())
+if check == 25:
+    print("Christmas")
+elif check == 24:
+    print("Christmas Eve")
+elif check == 23:
+    print("Christmas Eve Eve")
+elif check == 22:
+    print("Christmas Eve Eve Eve")
